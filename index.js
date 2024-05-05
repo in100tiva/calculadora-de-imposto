7c502ce1400cb85860007aeb2737cfa62ac1885b
in100tiva <projetoin100tiva@gmail.com>
2024-05-04 22:27:10 -0300

    versão 0.0.2
    melhorias na interface e a adição do tema claro

--- index.js
+++ index.js
@@ -52,3 +52,15 @@ function validarEntrada(valorHora, horasMes) {
     }
     return true;
 }
+
+// script.js
+function alternarTema() {
+    const body = document.body;
+    if (body.classList.contains('light-theme')) {
+        body.classList.remove('light-theme');
+        body.classList.add('dark-theme');
+    } else {
+        body.classList.remove('dark-theme');
+        body.classList.add('light-theme');
+    }
+}
